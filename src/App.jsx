f60833bee7e1cb2bddcf050e4949116e207a7792
Taztahir <taztaz162004@gmail.com>
2025-08-05 22:02:36 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,27 @@ import Header from './Header';
 import About from './About'
 import load from './assets/loader.gif';
 
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
     const spinner = document.getElementById("spinner");
     const content = document.getElementById("content");
 
-    // Show spinner for 2 seconds minimum
+    // Wait 2 seconds before starting fade-out
     setTimeout(() => {
       spinner.style.opacity = "0";
 
+      // Hide after fade-out animation
       setTimeout(() => {
         spinner.style.display = "none";
         content.classList.remove("hidden");
-      }, 500); // fade-out duration
-    }, 2000);
+      }, 500); // Matches fade duration
+    }, 2000); // 2 seconds delay
   });
 
 function App(){
   return(
-    <div  className='selection:bg-[#E68200] selection:text-white'>
-      <div className=''>
-        <div id='spinner' className='fixed flex justify-center items-center inset-0 w-full h-full z-[9999] bg-white'>
+    <div >
+      <div className='selection:bg-[#E68200] selection:text-white'>
+        <div id='spinner' className='fixed flex justify-center items-center inset-0 '>
           <img src={load} alt="" />
         </div>
       </div>
