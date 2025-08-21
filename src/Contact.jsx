8b7ee0683e99786588d8c694da5f7a2254c04bb5
Taztahir <taztaz162004@gmail.com>
2025-08-21 11:28:01 +0100

    Zayd

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -16,9 +16,9 @@ function Contact(){
                 <form action="https://formsubmit.co/taztaz162004@gmail.com" className="grid space-y-5 comic-neue-regular text-2xl" method="POST">
                     <input className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-60" type="text" name="firstname" id=""  placeholder="Your Firstname" required/>
                     <input className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-60" type="text" name="lastname" id=""  placeholder="Your Lastname" required/>
-                    <input className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-150" type="email" name="email" id="" placeholder="Your email address" required/>
-                    <input className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-150" type="text" name="subject" id=""  placeholder="Your Subject" required/>
-                    <textarea name="Messge" id="" placeholder="Say Somthing" className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-150 pb-40"></textarea>
+                    <input className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-130" type="email" name="email" id="" placeholder="Your email address" required/>
+                    <input className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-130" type="text" name="subject" id=""  placeholder="Your Subject" required/>
+                    <textarea name="Messge" id="" placeholder="Say Somthing" className="border-2 py-3 px-4 outline-[#ff9000] rounded-sm border-[#dfdfdf] lg:w-130 pb-40"></textarea>
                     <button className="comic-neue-bold bg-[#FF9000] text-white py-2 text-xl lg:w-40 rounded-lg lg:rounded-full">
                         Send message
                     </button>
