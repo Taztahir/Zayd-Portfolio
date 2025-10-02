db33a7e9682f9d5c98b5579f371f183fa39175d1
Taztahir <taztaz162004@gmail.com>
2025-10-03 00:40:36 +0100

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,12 +2,20 @@
 
 function Footer(){
     return(
-        <div className="md:flex text-center md:justify-between px-5 md:px-10 lg:px-20 py-6">
+        <div className="md:flex flex-col space-y-5 text-center md:justify-between px-5 md:px-10 bg-[#2A1454] lg:px-20 py-6">
             <div>
-                <h1>Copyright © 2025 <a href="" className="text-[#ff9000]">Zayd Tahir</a>. All right reserved </h1>
+                <h1 className="text-5xl text-white satisfy-regular">Zayd</h1>
+            </div>
+            <div className="text-white space-x-5 max-md:hidden">
+                <a className="hover:" href="#hero">Home</a>
+                <a className="hover:" href="#about">About</a>
+                <a className="hover:" href="#skills">Skills</a>
+                <a className="hover:" href="#project">Project</a>
+                <a className="hover:" href="#service">Service</a>
+                <a className="hover:" href="#resume">Resume</a>
             </div>
             <div>
-                <h1>Powered by: Zayd Tahir</h1>
+                <h1 className="text-gray-400">© 2024 All rights reserved by <a href="#hero" className="text-[#8750F7]">Zayd Tahir</a></h1>
             </div>
         </div>
     )
